61f034384ae5d167c2e5395cb5c52762b8e64d99
Turlough Cowman <tcowman@cognegen.com>
2024-04-19 12:40:17 +0100

    Temp fix for broken tiffs- uses tifffile library instead, but this is slower.

--- page_counter.py
+++ page_counter.py
@@ -4,45 +4,44 @@ import os
 import sys
 import concurrent.futures
 import concurrent
-from PIL import Image
+# from PIL import Image
 from tqdm import tqdm
 from exceptions import BrokenImageException
-from PyPDF2 import PdfFileReader
+from pypdf import PdfReader
+import tifffile
 
 fixpath = lambda path: str(Path(path.replace('"', '').strip()))
 
 
-def select_folder():
+def select_folder() -> str | None:
     """
     You can drag and drop a folders from Windows Explorer to the shell
     :return: whatever the user inputs, hopefully a directory
     """
     f = input("Type or drop a single folder here\n\t -> ")
-    if len(f) == 0:
-        exit(0)
-    return fixpath(f)
+    if not f.strip():
+        return None
+    f = f.replace('"', '').strip()
+    if not os.path.isdir(f):
+        return None
+    return f
 
 
-def get_files(parent_folders):
-    is_pdf = lambda f: f.lower().endswith('.pdf')
-    is_tif = lambda f: f.lower().endswith('.tif')
-    is_jpg = lambda f: f.lower().endswith('.jpg')
+def get_files(parent_folder: str) ->(list[str], list[str], list[str]):
 
     full_list = []
-    for parent_folder in parent_folders:
-        for folder, _, filenames in os.walk(parent_folder):
+    for folder, _, filenames in os.walk(parent_folder):
+        files = list(map(lambda f: os.path.join(folder, f), filenames))
+        full_list.extend(files)
 
-            files = list(map(lambda f: os.path.join(folder, f), filenames))
-            full_list.extend(files)
-
-    jpgs = [f for f in full_list if is_jpg(f)]
-    pdfs = [f for f in full_list if is_pdf(f)]
-    tifs = [f for f in full_list if is_tif(f)]
+    jpgs = [f for f in full_list if f.lower().endswith('.pdf')]
+    pdfs = [f for f in full_list if f.lower().endswith('.jpg')]
+    tifs = [f for f in full_list if f.lower().endswith('.tif')]
 
     return jpgs, pdfs, tifs
 
 
-def count_pdf(pdf):
+def count_pdf(pdf: str) -> int:
     """
     Counts the pages in a PDF.
     :param pdf: The pdf.
@@ -50,26 +49,26 @@ def count_pdf(pdf):
     """
     try:
         with open(pdf, 'rb') as f:
-            reader = PdfFileReader(f, strict=False)
-            return reader.numPages
+            reader = PdfReader(f, strict=False)
+            return reader.get_num_pages()
     except Exception as ex:
         raise BrokenImageException(pdf, ex)
 
 
-def count_tif(tif):
+def count_tif(tif: str) -> int:
     """
     Counts the number of pages in a TIF.
     :param tif: The TIFF file.
     :return: The number of pages.
     """
     try:
-        with Image.open(tif) as image:
-            return image.n_frames
+        with tifffile.TiffFile(tif) as tif:
+            return len(tif.pages)
     except Exception as ex:
         raise BrokenImageException(tif, ex)
 
 
-def count_list(counter, files):
+def count_list(counter, files: list[str]) -> (int, int):
     """
     Counts the number of pages in a file list, using the provided single page counter function
     :param counter: A function capable of counting the pages in an individual file of that type
@@ -94,33 +93,30 @@ def count_list(counter, files):
         return docs, pages
 
 
-def process_folders(folders):
+def process_folder(folder: str) -> None:
 
-    jpgs, pdfs, tifs = get_files(folders)
+    jpgs, pdfs, tifs = get_files(folder)
     print(f'\nFound {len(jpgs)} JPG documents, {len(pdfs)} PDF documents, {len(tifs)} TIF documents. Counting pages...')
 
-    if len(jpgs) > 0:
+    if jpgs:
         print('\nCounting JPGs')
         j_docs = len(jpgs)
     else:
         j_docs = 0
 
-    if len(pdfs) > 0:
+    if pdfs:
         print('\nCounting PDFs')
         p_docs, p_pages = count_list(count_pdf, pdfs)
     else:
         p_docs, p_pages = 0, 0
 
-    if len(tifs) > 0:
+    if tifs:
         print('\nCounting TIFFs')
         t_docs, t_pages = count_list(count_tif, tifs)
     else:
         t_docs, t_pages = 0, 0
 
     print()
-    print(f'Counted through {len(folders)} folders')
-    for f in folders:
-        print('\t', f)
     print(f'Totals:')
     print(f'\tJPG: {j_docs} documents, {j_docs} pages')
     print(f'\tPDF: {p_docs} documents, {p_pages} pages')
@@ -133,11 +129,14 @@ def process_folders(folders):
 
 def main():
     args = sys.argv[1:]
-    if len(args) > 0:
-        process_folders(args[0:])
+    if args:
+        process_folder(args[0])
 
-    while len(folder := select_folder()) > 0:
-        process_folders([folder])
+    while True:
+        if not (folder := select_folder()):
+            print("You must select a folder")
+            continue
+        process_folder(folder)
 
 
 if __name__ == '__main__':
---
+++ test_broken_tif.py
@@ -0,0 +1,5 @@
+import page_counter as pc
+
+count = pc.count_tif('broken.tif')
+print(count)
+
